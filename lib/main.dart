1f25767a5a068a4f63f6470878e5e278f6487180
RubaZyada <robazyada167@gmail.com>
2025-09-25 16:15:44 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         Routes.main: (context) => MainApp(),
         Routes.freelancerDetails: (context) => FreelancerDetails(),
       },
-      home: MainApp(),
+      home: Login(),
     
     );
     
