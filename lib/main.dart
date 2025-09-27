256ace05ba7ccb196b3db0e19a6d923d188b31b1
RubaZyada <robazyada167@gmail.com>
2025-09-27 20:40:49 +0300

    SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,37 @@ import 'package:gsg_flutter/screens/home.dart';
 import 'package:gsg_flutter/screens/login.dart';
 import 'package:gsg_flutter/screens/main_app.dart';
 import 'package:gsg_flutter/screens/signup.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 //import 'package:gsg_flutter/products/product_details.dart';
 //import 'package:gsg_flutter/screens/signup.dart';
 //import 'package:gsg_flutter/screens/main_app.dart';
 
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  // استدعي هدا السطر عشان ال async in main
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs= await SharedPreferences.getInstance();
+  String ? userEmail = prefs.getString(Login.userCredentialKey);
+ 
+ runApp(const MyApp());
+//  runApp(MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       routes: {
+//         Routes.login: (context) => Login(),
+//         Routes.home: (context) => Home(),
+//         Routes.signup: (context) => Signup(),
+//         Routes.main: (context) => MainApp(),
+//         Routes.freelancerDetails: (context) => FreelancerDetails(),
+//         Routes.mainNavScreen: (context) => MainNavScreen(),
+//       },
+//       //shared preferences
+//      home: userEmail != null ? Home(name: userEmail,) : Login(),
+    
+//     ));
 }
 class MyApp extends StatelessWidget {
+  
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,8 +50,7 @@ class MyApp extends StatelessWidget {
         Routes.freelancerDetails: (context) => FreelancerDetails(),
         Routes.mainNavScreen: (context) => MainNavScreen(),
       },
-     home: CounterScreen(),
-    
+    // home: userEmail!= null ? Home(name: userEmail,) : Login(),  
     );
     
   }
